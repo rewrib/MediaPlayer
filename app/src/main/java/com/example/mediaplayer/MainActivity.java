2a9622a0b1e6329aabee1c039465633e9e1fda3a
rewrib <birwer.ali@gmx.de>
2019-10-28 16:22:12 +0100

    showing a toast message at the end of a song

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private MediaPlayer mediaPlayer;
@@ -19,6 +21,19 @@ private MediaPlayer mediaPlayer;
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+
+            }
+        });
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Context context = getApplicationContext();
+                CharSequence text = "I have completed my task.";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
             }
         });
         Button pauseButton = findViewById(R.id.pause_button);
